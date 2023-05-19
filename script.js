3bd64fa1076f91afed69e811189c95ea4fe66a65
Vicente Vázquez <128206250+vivaz074@users.noreply.github.com>
2023-05-18 23:43:00 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-// Obtener todas las imágenes del carrusel
-const carouselImages = document.querySelectorAll(".carousel-image");
 
-// Establecer la primera imagen como activa
-let activeImageIndex = 0;
-carouselImages[activeImageIndex].classList.add("active");
 
-// Función para pasar a la siguiente imagen
-function nextImage() {
-  carouselImages[activeImageIndex].classList.remove("active");
-  activeImageIndex = (activeImageIndex + 1) % carouselImages.length;
-  carouselImages[activeImageIndex].classList.add("active");
-}
+const servicios = {
+  A: ["Adenomiosis", "Amenorrea primaria", "Amenorrea secundaria", "Aborto incompleto", "Aborto inevitable", "Aborto séptico", "Aborto espontáneo", "Absceso de Bartolino", "Absceso en la glándula areolar", "Adherencias uterinas", "Amenaza de aborto", "Ausencia de la menstruación"],
+  B: ["Bacteriuria asintomática"],
+  C: ["Cáncer de mama", "Cáncer de los ovarios", "Cáncer de útero", "Cáncer del cuello uterino", "Cáncer endometrial", "Candidiasis vaginal", "Cambios precancerosos del cuello uterino", "Cervicitis", "Chancroide", "Citomegalovirus (CMV)", "Climaterio y menopausia", "Cistitis", "Condiloma acuminado"],
+  D: ["Diabetes gestacional", "Desminución del deseo sexual", "Disminución del flujo vaginal - leucorrea", "Displasia cervical", "Dolor asociado con la ovulación"],
+  E: ["Esterilidad femenina", "Embarazo anembrionario (Huevo huero)", "Embarazo de alto riesgo", "Embarazo molar", "Embarazo normal", "Endometriosis", "Enfermedad benigna de las mamas", "Enfermedad de los ovarios poliquísticos", "Enfermedad fibroquística de las mamas", "Enfermedad inflamatoria pélvica (EIP)", "Enfermedad ovárica poliquística", "Enfermedad trofoblástica gestacional", "Enfermedades de la glándula mamaria", "Enfermedad de transmisión sexual femenina (ETS)", "Exceso de flujo vaginal - leucorrea"],
+  F: ["Fibroadenoma de mama"],
+  G: ["Glándula de Bartolino"],
+  H: ["Hematoma retroplacentario", "Herpes genital", "Hiperandrogenismo", "Hipertensión inducida por el embarazo", "Hiperplasia endometrial benigna"],
+  I: ["Infertilidad", "Infección de la vagina por levaduras", "Infección del tejido mamario", "Infección mamaria", "Infecciones por clamidia en mujeres", "Infección de vías urinarias", "Insuficiencia ovárica prematura", "Interrupción legal del embarazo"],
+  M: ["Mastopatia fibroquística", "Menopausia", "Miomas uterinos"],
+  O: ["Obesidad en el embarazo", "Ovarios poliquísticos"],
+  P: ["Pérdida gestacional recurrente", "Preeclampsia"],
+  Q: ["Quiste del conducto de Gartner", "Quistes ováricos fisiológicos", "Quistes ováricos funcionales"],
+  S: ["Salpingitis", "Sangrado uterino anormal", "Sangrado uterino disfuncional", "Síndrome de hiperestimulación ovárica", "Síndrome de los ovarios poliquísticos (PCOS)"],
+  T: ["Trastornos del desarrollo sexual", "Trastorno disfórico premenstrual (TDPM)"]
+};
 
-// Iniciar el carrusel y cambiar las imágenes cada 5 segundos
-setInterval(nextImage, 5000);
+document.addEventListener("DOMContentLoaded", function() {
+  const botonesServicio = document.querySelectorAll(".servicio-btn");
+  const listaServicios = document.getElementById("lista-servicios");
 
-// ...
-
-const servicios = [
-  "Embarazo normal",
-  "Embarazo de alto riesgo",
-  "Miomas uterinos",
-  "Menopausia",
-  "Virus del papiloma humano (VPH)",
-  // Continuar con el resto de los servicios en la lista
-];
-
-document.querySelectorAll(".circulo-btn").forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const target = e.target.dataset.target;
-    const filteredServicios = servicios.filter((servicio) =>
-      servicio.startsWith(target)
-    );
-
-    const serviciosList = document.querySelector("#servicios-lista");
-    serviciosList.innerHTML = "";
-    filteredServicios.forEach((servicio) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = servicio;
-      serviciosList.appendChild(listItem);
+  botonesServicio.forEach(btn => {
+    btn.addEventListener("click", function() {
+      const letra = this.getAttribute("data-letra");
+      const serviciosConLetra = servicios[letra];
+      
+      let html = `<h4>Servicios que empiezan con ${letra}</h4>`;
+      html += "<ul>";
+      
+      serviciosConLetra.forEach(servicio => {
+        html += `<li>${servicio}</li>`;
+      });
+      
+      html += "</ul>";
+      
+      listaServicios.innerHTML = html;
     });
   });
 });
 
+$('.carousel').on('slid.bs.carousel', function (e) {
+  var href = $(e.relatedTarget).find('img').attr('src');
+  $('.expand-icon').attr('href', href).attr('data-lightbox', 'carousel');
+});
+
+lightbox.option({
+  'alwaysShowNavOnTouchDevices': true,
+  'wrapAround': true
+});
 
 
-  
+$('.expand-icon').on('click', function (e) {
+  e.preventDefault();
+});
+
+
+
+$(window).on('load resize', function() {
+  var carouselHeight = $('#carouselExampleIndicators .carousel-item.active img').height();
+  $('#carousel-indicators').css('bottom', (carouselHeight + 10) + 'px');
+});
+
+$('.carousel').on('slide.bs.carousel', function() {
+  var carouselHeight = $('.carousel-item.active img').height();
+  $('#carousel-indicators').css('bottom', (carouselHeight + 10) + 'px');
+});
